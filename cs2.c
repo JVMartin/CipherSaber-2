8c3b7c87a1f58c36501c2fb22eb47d738fe648d2
Jacob Martin <mindnumb@gmail.com>
2015-11-22 22:06:46 -0800

    Get file contents

--- cs2.c
+++ cs2.c
@@ -5,29 +5,35 @@
 #define ROUNDS 20
 
 int checkCommand(int argc, char ** argv);
-void initState(unsigned char * state, char * key, int keyLength);
+void initState(unsigned char * state, char * key);
+void decrypt(unsigned char * state, char * key,
+	char * srcPath, char * destPath);
+char * getFileContents(char * path);
+//void decrypt();
 
 int main(int argc, char ** argv)
 {
 	if ( ! checkCommand(argc, argv)) {
 		printf("Usage:\n");
-		printf("	cs2 encrypt <key> <input file> <output file>\n");
-		printf("	cs2 decrypt <key> <input file> <output file>\n");
+		printf("	cs2 encrypt <key> <source path> <dest path>\n");
+		printf("	cs2 decrypt <key> <source path> <dest path>\n");
 		return 0;
 	}
 
 	unsigned char * state;
 	char * key;
-	int keyLength;
 
 	state = (unsigned char *) malloc(256);
 	key   = (char *)          malloc(256);
 
 	// Grab the key from the argument.
 	strcpy(key, argv[2]);
-	keyLength = strlen(key);
 
-	initState(state, key, keyLength + 10);
+	if (strcmp(argv[1], "decrypt") == 0) {
+		decrypt(state, key, argv[3], argv[4]);
+	}
+	else {
+	}
 
 	free(state);
 	free(key);
@@ -51,10 +57,12 @@ int checkCommand(int argc, char ** argv)
 	return 1;
 }
 
-void initState(unsigned char * state, char * key, int keyLength)
+void initState(unsigned char * state, char * key)
 {
 	unsigned char c;
-	int i, j, n;
+	int i, j, n, keyLength;
+
+	keyLength = strlen(key);
 
 	// Initialize the state array.
 	for (i = 0; i < 255; ++i) {
@@ -75,4 +83,43 @@ void initState(unsigned char * state, char * key, int keyLength)
 			state[j] = c;
 		}
 	}
+}
+
+void decrypt(unsigned char * state, char * key,
+	char * srcPath, char * destPath)
+{
+	FILE * destFile;
+
+	char * contents = getFileContents(srcPath);
+	printf("%s\n", contents);
+	free(contents);
+}
+
+char * getFileContents(char * path)
+{
+	FILE * file;
+	int fileSize;
+	char * data;
+
+	file = fopen(path, "r");
+	if ( ! file) {
+		printf("Could not open file: %s\n", path);
+		return;
+	}
+
+	fseek(file, 0L, SEEK_END);
+	fileSize = ftell(file);
+	rewind(file);
+
+	printf("File size: %d bytes.\n", fileSize);
+
+	data = malloc(fileSize);
+	if ( ! data) {
+		printf("Could not allocate enough memory.\n");
+		return;
+	}
+
+	fread(data, fileSize, 1, file);
+	fclose(file);
+	return data;
 }
