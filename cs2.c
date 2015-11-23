3f825c99e23cfa1464511c7fbdf5aa76f253a873
Jacob Martin <mindnumb@gmail.com>
2015-11-22 20:50:30 -0800

    Initialize the state array

--- cs2.c
+++ cs2.c
@@ -2,16 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ROUNDS 20
+
 int checkCommand(int argc, char ** argv);
+void initState(unsigned char * state, char * key, int keyLength);
 
 int main(int argc, char ** argv)
 {
 	if ( ! checkCommand(argc, argv)) {
 		printf("Usage:\n");
-		printf("	cs2 encrypt <password> <input file> <output file>\n");
-		printf("	cs2 decrypt <password> <input file> <output file>\n");
+		printf("	cs2 encrypt <key> <input file> <output file>\n");
+		printf("	cs2 decrypt <key> <input file> <output file>\n");
+		return 0;
 	}
 
+	unsigned char * state;
+	char * key;
+	int keyLength;
+
+	state = (unsigned char *) malloc(256);
+	key   = (char *)          malloc(256);
+
+	// Grab the key from the argument.
+	strcpy(key, argv[2]);
+	keyLength = strlen(key);
+
+	initState(state, key, keyLength + 10);
+
+	free(state);
+	free(key);
 	return 0;
 }
 
@@ -30,4 +49,30 @@ int checkCommand(int argc, char ** argv)
 	}
 	
 	return 1;
+}
+
+void initState(unsigned char * state, char * key, int keyLength)
+{
+	unsigned char c;
+	int i, j, n;
+
+	// Initialize the state array.
+	for (i = 0; i < 255; ++i) {
+		state[i] = i;
+	}
+
+	j =  0;
+
+	for (n = 0; n < ROUNDS; ++n) {
+		for (i = 0; i < 255; ++i) {
+			j += state[i];
+			j += key[i % keyLength];
+			j = j % 256;
+
+			// Swap the ith and jth elements of the state array.
+			c = state[i];
+			state[i] = state[j];
+			state[j] = c;
+		}
+	}
 }
