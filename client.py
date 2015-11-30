120a3a6abf3039b1278ac0b066cacc431a2a0fad
Jacob Martin <mindnumb@gmail.com>
2015-11-29 23:33:52 -0800

    Allow dynamic messages

--- client.py
+++ client.py
@@ -4,13 +4,23 @@
 
 # A client for sending TauNet Messages.
 
+import sys
 import socket
 
+args = sys.argv
+
+# Helpful usage hints.
+if len(args) != 2:
+	print("usage:")
+	print("\tpython3 client.py <message>\n")
+	exit()
+
+message = args[1]
 
 target_host = "localhost"
 target_port = 6283
 
 target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 target.connect((target_host, target_port))
-target.send("Hello there.".encode())
+target.send(message.encode())
 target.close()
