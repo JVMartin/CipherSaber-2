18739b234889505fee096a815042a838f974f54f
Jacob Martin <mindnumb@gmail.com>
2015-12-08 15:33:41 -0800

    Decided on different indent for multi-line if statement

--- receive.py
+++ receive.py
@@ -45,11 +45,11 @@ while True:
 
 		# Check if the message is malformed or noncompliant.
 		if (
-				len(lines) < 4 or
-				lines[0] != "version: 0.2" or
-				not lines[1].startswith("from: ") or
-				not lines[2].startswith("to: ") or
-				len(lines[3]) # The fourth line must be empty
+			len(lines) < 4 or
+			lines[0] != "version: 0.2" or
+			not lines[1].startswith("from: ") or
+			not lines[2].startswith("to: ") or
+			len(lines[3]) # The fourth line must be empty
 		   ):
 			print("Received malformed message from " + str(from_address))
 			print("Malformed message:")
